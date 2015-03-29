a76c9b15ada4d8de490310e3007a7ecf5b587d06
Sajan <sajangupta74@gmail.com>
2015-03-29 22:14:47 +0530

    "user model update"

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   def self.from_omniauth(auth)
-    error = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    #last_id = User.last.id
+    error = where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      #user.id = last_id + 1
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
       user.encrypted_password = Devise.friendly_token[0,20]
+      user.reset_password_token = Devise.friendly_token[0,10]
       user.sign_in_count = user.sign_in_count + 1
       user.current_sign_in_at = DateTime.now
       user.last_sign_in_at = DateTime.now
     end
-    error.errors.full_messages
+    puts "error is #{error.inspect}  END"
   end
 end
+
+
